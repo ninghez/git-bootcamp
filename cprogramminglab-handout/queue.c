9fa1fbdaa6c309cf26bd789ab1dfed963d496e3f
Ninghe Zhang <ninghez@andrew.cmu.edu>
2019-01-22 19:03:50 -0500

    queue.c change 1

--- cprogramminglab-handout/queue.c
+++ cprogramminglab-handout/queue.c
@@ -7,7 +7,7 @@
 /*
  * This program implements a queue supporting both FIFO and LIFO
  * operations.
- *
+ *SADANSJDASL
  * It uses a singly-linked list to represent the set of queue elements
  */
 
